dc334c338ca685ece9ef778f810f3ce6b90d5235
amiko morgoshia <morgoshiaaamiko@gmail.com>
2026-06-09 00:26:21 +0400

    feat: implement CountNumbersInPowerRange function

--- Homework_8/Program.cs
+++ Homework_8/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var result = CountNumbersInPowerRange(2, 27, 4);
+            Console.WriteLine(result);
+        }
+
+        public static int CountNumbersInPowerRange(int a, int b, int n) {
+            var count = 0;
+
+            for (var i = 0; i <= b; i++) {
+                double powerValue = Math.Pow(i, n);
+                if (powerValue >= a && powerValue <= b)
+                {
+                    count++;
+                }
+
+                if (powerValue > b) {
+                    break;
+                }
+            }
+
+            return count;
         }
     }
 }
