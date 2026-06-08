df1df9466e3050db6f83dd47dcada0ada232688e
amiko morgoshia <morgoshiaaamiko@gmail.com>
2026-06-09 00:38:54 +0400

    feat: implement sock pair counter logic

--- Homework_8/Program.cs
+++ Homework_8/Program.cs
@@ -4,8 +4,12 @@
     {
         static void Main(string[] args)
         {
-            var result = CountNumbersInPowerRange(2, 27, 4);
+            //var result = CountNumbersInPowerRange(2, 27, 4);
+            //Console.WriteLine(result);
+
+            var result = CountSockPairs("AAABB");
             Console.WriteLine(result);
+
         }
 
         public static int CountNumbersInPowerRange(int a, int b, int n) {
@@ -24,6 +28,26 @@
             }
 
             return count;
+        }
+
+        public static int CountSockPairs(string socks)
+        {
+            char[] charArray = socks.ToCharArray();
+            Array.Sort(charArray);
+            var sortedSocks = new string(charArray);
+
+            var pairs = 0;
+
+            for (var i = 0; i < sortedSocks.Length - 1; i++) {
+                if (sortedSocks[i] == sortedSocks[i + 1]) {
+                    pairs++;
+                    i++;
+                }
+            }
+
+            return pairs;
+
+
         }
     }
 }
